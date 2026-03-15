13b53524d9e672023387789cb110a4422ea2c5a8
Daniel Tucker <code@dantuck.com>
2026-03-15 10:29:36 -0600

    refactor: convert index/tags pages to consistent structure, fix Header-in-head bug, self-contain Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,56 +1,69 @@
 <script>
-    // console.log(window.ResizeObserver)
-    import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
-    onMount(() => {
-      handleObserver();
-    });
+  onMount(() => {
+    handleObserver();
+  });
 
-    const handleObserver = () => {
-      if (document.querySelector("header nav #nav")) {
-        var progress = document.getElementById("reading-progress");
+  const handleObserver = () => {
+    if (document.querySelector("header nav #nav")) {
+      var progress = document.getElementById("reading-progress");
+      var timeOfLastScroll = 0;
+      var requestedAniFrame = false;
 
-        var timeOfLastScroll = 0;
-        var requestedAniFrame = false;
-        function scroll() {
-          if (!requestedAniFrame) {
-            requestAnimationFrame(updateProgress);
-            requestedAniFrame = true;
-          }
-          timeOfLastScroll = Date.now();
+      function scroll() {
+        if (!requestedAniFrame) {
+          requestAnimationFrame(updateProgress);
+          requestedAniFrame = true;
         }
-        addEventListener("scroll", scroll);
-
-        var winHeight = 1000;
-        var bottom = 10000;
-        function updateProgress() {
-          requestedAniFrame = false;
-          var percent = Math.min(
-            (document.scrollingElement.scrollTop / (bottom - winHeight)) * 100,
-            100
-          );
-          progress.style.transform = `translate(-${100 - percent}vw, 0)`;
-          if (Date.now() - timeOfLastScroll < 3000) {
-            requestAnimationFrame(updateProgress);
-            requestedAniFrame = true;
-          }
-        }
-
-        new ResizeObserver(() => {
-          bottom =
-            document.scrollingElement.scrollTop +
-            document.querySelector("#comments,footer").getBoundingClientRect().top;
-          winHeight = window.innerHeight;
-          scroll();
-        }).observe(document.body);
+        timeOfLastScroll = Date.now();
       }
+      addEventListener("scroll", scroll);
+
+      var winHeight = 1000;
+      var bottom = 10000;
+
+      function updateProgress() {
+        requestedAniFrame = false;
+        var percent = Math.min(
+          (document.scrollingElement.scrollTop / (bottom - winHeight)) * 100,
+          100
+        );
+        progress.style.transform = `translate(-${100 - percent}vw, 0)`;
+        if (Date.now() - timeOfLastScroll < 3000) {
+          requestAnimationFrame(updateProgress);
+          requestedAniFrame = true;
+        }
+      }
+
+      new ResizeObserver(() => {
+        bottom =
+          document.scrollingElement.scrollTop +
+          document.querySelector("#comments,footer").getBoundingClientRect().top;
+        winHeight = window.innerHeight;
+        scroll();
+      }).observe(document.body);
     }
-
-
-
+  };
 </script>
 
-<footer>
-    <a rel="me" href="https://fosstodon.org/@tuck">@tuck@fosstodon.org | Mastodon</a>
-    <div id="reading-progress" aria-hidden="true"></div>
+<footer class="max-content py-12 text-center text-[90%]">
+  <a rel="me" href="https://fosstodon.org/@tuck">@tuck@fosstodon.org | Mastodon</a>
+  <div id="reading-progress" aria-hidden="true"></div>
 </footer>
+
+<style>
+  #reading-progress {
+    z-index: 1;
+    background-color: rgb(255, 140, 0);
+    width: 100vw;
+    height: 0.5rem;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    transform: translate(-100vw, 0);
+    will-change: transform;
+    pointer-events: none;
+    margin: 0;
+  }
+</style>
